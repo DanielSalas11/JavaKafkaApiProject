be34785d3caa7674474386c7f3bc19083eaacbe7
DanielSalas11 <daniel_salas11@outlook.com>
2025-09-08 19:35:15 -0700

    Created project with Spring initializr, added .gitignore file

---
+++ JavaKafkaApiApplication/src/main/java/com/danielsalas/JavaKafkaApiApplication/JavaKafkaApiApplication.java
@@ -0,0 +1,13 @@
+package com.danielsalas.JavaKafkaApiApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKafkaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaKafkaApiApplication.class, args);
+	}
+
+}
---
+++ JavaKafkaApiApplication/src/test/java/com/danielsalas/JavaKafkaApiApplication/JavaKafkaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielsalas.JavaKafkaApiApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaKafkaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
